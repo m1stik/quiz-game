36ca3c46a5f2b6a3e0300ec325ac59ac63e75200
m1stik <ivanday221@gmail.com>
2022-04-05 13:48:49 +0300

    Cleaning and commenting the code

--- data.py
+++ data.py
@@ -1,17 +1,15 @@
-#amount 10
-#type boolean
-#https://opentdb.com/api.php
-
 import requests
 
+# Parameters for the further API request
 params = {
     "amount": 10,
     "type": "boolean"
 }
 
+# Getting data from the API
 response = requests.get("https://opentdb.com/api.php", params=params)
 response.raise_for_status()
 
+# Store the data
 data = response.json()
-
 question_data = data["results"]
--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from quiz_brain import QuizBrain
 from ui import QuizInterface
 
 question_bank = []
+# Fetching data
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
+# Initializing quiz with the provided data
 quiz = QuizBrain(question_bank)
 
+# Displaying the quiz
 quiz_ui = QuizInterface(quiz)
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
