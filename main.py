b0663459f46143aebc1a0e563e927f2514cc7920
m1stik <ivanday221@gmail.com>
2022-04-05 13:41:12 +0300

    Adding GUI

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-import random
+from ui import QuizInterface
 
 question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-# Taking questions from data-file and putting them into array
-for item in question_data:
-    question_bank.append(Question(item["text"], item["answer"]))
 
-# Shuffling questions in the array and initializing the brain of the quiz
-random.shuffle(question_bank)
 quiz = QuizBrain(question_bank)
 
-# Displaying questions and results
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface(quiz)
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,31 +1,26 @@
+import html
+
 class QuizBrain:
-    # Initial attrabutes of the class
+
     def __init__(self, q_list):
         self.question_number = 0
-        self.question_list = q_list
         self.score = 0
+        self.question_list = q_list
+        self.current_question = None
 
-    # Checking if there are free questions in the array
     def still_has_questions(self):
-        if self.question_number > len(self.question_list) - 1:
-            print(f"The quiz is over. Your final score: {self.score} / {self.question_number}")
-            return False
-        else:
-            return True
+        return self.question_number < len(self.question_list)
 
-    # Displaying a question and handling user's input
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer)
+        self.q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {self.q_text}"
 
-    # Checking user's answer and displaying results
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            print("You got it right!")
             self.score += 1
+            return True
         else:
-            print("That's wrong.")
-        print(f"The correct answer was: {correct_answer}.")
-        print(f"Your current score is: {self.score} / {self.question_number}\n")
+            return False
